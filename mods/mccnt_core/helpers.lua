b45bfd06489f0b208255f44fcb73adb81edfe630
ROllerozxa <temporaryemail4meh+github@gmail.com>
2022-03-20 22:00:10 +0100

    Add sounds.

--- mods/mccnt_core/helpers.lua
+++ mods/mccnt_core/helpers.lua
@@ -11,7 +11,31 @@ function terrain(id)
 	return atlas_id("terrain", id)
 end
 
+-- Returns a table with sounds for the given material.
 function block_sound(material)
-	-- TODO
-	return nil
+	-- TODO: metal sounds
+	if material == "metal" then
+		material = "stone"
+	end
+
+	local table = {
+		footstep = {
+			name = material,
+			gain = 0.5
+		},
+		dig = {
+			name = material,
+			gain = 1.0
+		},
+		dug = {
+			name = material,
+			gain = 1.0
+		},
+		place = {
+			name = material,
+			gain = 1.0
+		}
+	}
+
+	return table
 end
