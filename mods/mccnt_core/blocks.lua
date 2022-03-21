64fd0ea3500418a8a0ca4f2b50df735d0441ae56
ROllerozxa <temporaryemail4meh+github@gmail.com>
2022-03-21 21:43:09 +0100

    Shorten sound definition.

--- mods/mccnt_core/blocks.lua
+++ mods/mccnt_core/blocks.lua
@@ -4,7 +4,7 @@ local blocks = {
 		order = 1,
 		description = "Stone",
 		tiles = { terrain(1) },
-		sounds = block_sound('stone'),
+		sound = 'stone',
 	},
 	grass = {
 		order = 2,
@@ -14,25 +14,25 @@ local blocks = {
 			terrain(2),
 			terrain(3),
 		},
-		sounds = block_sound('grass'),
+		sound = 'grass',
 	},
 	dirt = {
 		order = 3,
 		description = "Dirt",
 		tiles = { terrain(2) },
-		sounds = block_sound('gravel'),
+		sound = 'gravel',
 	},
 	cobblestone = {
 		order = 4,
 		description = "Cobblestone",
 		tiles = { terrain(16) },
-		sounds = block_sound('stone'),
+		sound = 'stone',
 	},
 	wood = {
 		order = 5,
 		description = "Wood",
 		tiles = { terrain(4) },
-		sounds = block_sound('wood'),
+		sound = 'wood',
 	},
 	sapling = {
 		order = 6,
@@ -44,13 +44,13 @@ local blocks = {
 		paramtype = "light",
 		sunlight_propagates = true,
 		walkable = false,
-		sounds = block_sound('grass'),
+		sound = 'grass',
 	},
 	bedrock = {
 		order = 7,
 		description = "Bedrock",
 		tiles = { terrain(17) },
-		sounds = block_sound('stone'),
+		sound = 'stone',
 	},
 	water = {
 		order = 8,
@@ -101,31 +101,31 @@ local blocks = {
 		order = 12,
 		description = "Sand",
 		tiles = { terrain(18) },
-		sounds = block_sound('sand'),
+		sound = 'sand',
 	},
 	gravel = {
 		order = 13,
 		description = "Gravel",
 		tiles = { terrain(19) },
-		sounds = block_sound('gravel'),
+		sound = 'gravel',
 	},
 	gold_ore = {
 		order = 14,
 		description = "Gold Ore",
 		tiles = { terrain(32) },
-		sounds = block_sound('stone'),
+		sound = 'stone',
 	},
 	iron_ore = {
 		order = 15,
 		description = "Iron Ore",
 		tiles = { terrain(33) },
-		sounds = block_sound('stone'),
+		sound = 'stone',
 	},
 	coal_ore = {
 		order = 16,
 		description = "Coal Ore",
 		tiles = { terrain(34) },
-		sounds = block_sound('stone'),
+		sound = 'stone',
 	},
 	log = {
 		order = 17,
@@ -135,7 +135,7 @@ local blocks = {
 			"terrain.png^[sheet:16x16:5,1",
 			"terrain.png^[sheet:16x16:4,1"
 		},
-		sounds = block_sound('wood'),
+		sound = 'wood',
 	},
 	leaves = {
 		order = 18,
@@ -144,13 +144,13 @@ local blocks = {
 		tiles = { terrain(22) },
 		paramtype = "light",
 		waving = 2,
-		sounds = block_sound('grass'),
+		sound = 'grass',
 	},
 	sponge = {
 		order = 19,
 		description = "Sponge",
 		tiles = { terrain(48) },
-		sounds = block_sound('grass'),
+		sound = 'grass',
 	},
 	glass = {
 		order = 20,
@@ -159,104 +159,104 @@ local blocks = {
 		tiles = { terrain(49) },
 		paramtype = "light",
 		sunlight_propagates = true,
-		sounds = block_sound('glass'),
+		sound = 'glass',
 	},
 
 	red_wool = {
 		order = 21,
 		description = "Red Wool",
 		tiles = { terrain(64) },
-		sounds = block_sound('cloth'),
+		sound = 'cloth',
 	},
 	orange_wool = {
 		order = 22,
 		description = "Orange Wool",
 		tiles = { terrain(65) },
-		sounds = block_sound('cloth'),
+		sound = 'cloth',
 	},
 	yellow_wool = {
 		order = 23,
 		description = "Yellow Wool",
 		tiles = { terrain(66) },
-		sounds = block_sound('cloth'),
+		sound = 'cloth',
 	},
 	lime_wool = {
 		order = 24,
 		description = "Lime Wool",
 		tiles = { terrain(67) },
-		sounds = block_sound('cloth'),
+		sound = 'cloth',
 	},
 	green_wool = {
 		order = 25,
 		description = "Green Wool",
 		tiles = { terrain(68) },
-		sounds = block_sound('cloth'),
+		sound = 'cloth',
 	},
 	teal_wool = {
 		order = 26,
 		description = "Teal Wool",
 		tiles = { terrain(69) },
-		sounds = block_sound('cloth'),
+		sound = 'cloth',
 	},
 	aqua_wool = {
 		order = 27,
 		description = "Aqua Wool",
 		tiles = { terrain(70) },
-		sounds = block_sound('cloth'),
+		sound = 'cloth',
 	},
 	cyan_wool = {
 		order = 28,
 		description = "Cyan Wool",
 		tiles = { terrain(71) },
-		sounds = block_sound('cloth'),
+		sound = 'cloth',
 	},
 	blue_wool = {
 		order = 29,
 		description = "Blue Wool",
 		tiles = { terrain(72) },
-		sounds = block_sound('cloth'),
+		sound = 'cloth',
 	},
 	indigo_wool = {
 		order = 30,
 		description = "Indigo Wool",
 		tiles = { terrain(73) },
-		sounds = block_sound('cloth'),
+		sound = 'cloth',
 	},
 	violet_wool = {
 		order = 31,
 		description = "Violet Wool",
 		tiles = { terrain(74) },
-		sounds = block_sound('cloth'),
+		sound = 'cloth',
 	},
 	magenta_wool = {
 		order = 32,
 		description = "Magenta Wool",
 		tiles = { terrain(75) },
-		sounds = block_sound('cloth'),
+		sound = 'cloth',
 	},
 	pink_wool = {
 		order = 33,
 		description = "Pink Wool",
 		tiles = { terrain(76) },
-		sounds = block_sound('cloth'),
+		sound = 'cloth',
 	},
 	black_wool = {
 		order = 34,
 		description = "Black Wool",
 		tiles = { terrain(77) },
-		sounds = block_sound('cloth'),
+		sound = 'cloth',
 	},
 	gray_wool = {
 		order = 35,
 		description = "Gray Wool",
 		tiles = { terrain(78) },
-		sounds = block_sound('cloth'),
+		sound = 'cloth',
 	},
 	white_wool = {
 		order = 36,
 		description = "White Wool",
 		tiles = { terrain(79) },
-		sounds = block_sound('cloth'),
+		sound = 'cloth',
 	},
 
 	red_flower = {
@@ -269,7 +269,7 @@ local blocks = {
 		tiles = { terrain(13) },
 		inventory_image = terrain(13),
 		waving = 1,
-		sounds = block_sound('grass'),
+		sound = 'grass',
 	},
 
 	yellow_flower = {
@@ -282,7 +282,7 @@ local blocks = {
 		tiles = { terrain(12) },
 		inventory_image = terrain(12),
 		waving = 1,
-		sounds = block_sound('grass'),
+		sound = 'grass',
 	},
 
 	brown_mushroom = {
@@ -295,7 +295,7 @@ local blocks = {
 		tiles = { terrain(29) },
 		inventory_image = terrain(29),
 		waving = 1,
-		sounds = block_sound('grass'),
+		sound = 'grass',
 	},
 
 	red_mushroom = {
@@ -308,7 +308,7 @@ local blocks = {
 		tiles = { terrain(28) },
 		inventory_image = terrain(28),
 		waving = 1,
-		sounds = block_sound('grass'),
+		sound = 'grass',
 	},
 
 	gold_block = {
@@ -322,7 +322,7 @@ local blocks = {
 			terrain(40),
 			terrain(40),
 		},
-		sounds = block_sound('metal'),
+		sound = 'metal',
 	},
 
 	iron_block = {
@@ -336,7 +336,7 @@ local blocks = {
 			terrain(39),
 			terrain(39),
 		},
-		sounds = block_sound('metal'),
+		sound = 'metal',
 	},
 
 	stone_slab = {
@@ -350,7 +350,6 @@ local blocks = {
 			terrain(5),
 			terrain(5),
 		},
-		is_ground_content = false,
 		drawtype = "nodebox",
 		paramtype = "light",
 		node_box = {
@@ -359,14 +358,14 @@ local blocks = {
 				{-0.5, -0.5, -0.5, 0.5, 0, 0.5},
 			}
 		},
-		sounds = block_sound('stone'),
+		sound = 'stone',
 	},
 
 	bricks = {
 		order = 45,
 		description = "Bricks",
 		tiles = { terrain(7) },
-		sounds = block_sound('stone'),
+		sound = 'stone',
 	},
 	tnt = {
 		order = 46,
@@ -376,7 +375,7 @@ local blocks = {
 			terrain(10),
 			terrain(8),
 		},
-		sounds = block_sound('grass'),
+		sound = 'grass',
 	},
 	bookshelf = {
 		order = 47,
@@ -386,31 +385,34 @@ local blocks = {
 			terrain(4),
 			terrain(35),
 		},
-		sounds = block_sound('wood'),
+		sound = 'wood',
 	},
 	mossy_cobblestone = {
 		order = 48,
 		description = "Mossy Cobblestone",
 		tiles = { terrain(36) },
-		sounds = block_sound('stone'),
+		sound = 'stone',
 	},
 	obsidian = {
 		order = 49,
 		description = "Obsidian",
 		tiles = { terrain(37) },
-		sounds = block_sound('stone'),
+		sound = 'stone',
 	},
 }
 
-local i = 1
 for name, def in pairs(blocks) do
 	if not def.groups then def.groups = {} end
 	def.groups.instantly = 3
 
 	def.drop = ""
 
-	def._mccnt_order = i
-	i = i + 1
+	def.stack_max = 1
+
+	if def.sound then
+		def.sounds = block_sound(def.sound)
+		def.sound = nil
+	end
 
 	minetest.register_node(":minecraft:"..name, def)
 end
