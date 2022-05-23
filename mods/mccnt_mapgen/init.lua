d895e19392ce493487dd11a771fdd17dec0cbd73
ROllerozxa <temporaryemail4meh+github@gmail.com>
2022-05-23 10:46:46 +0200

    Optimise mapgen

--- mods/mccnt_mapgen/init.lua
+++ mods/mccnt_mapgen/init.lua
@@ -44,45 +44,39 @@ minetest.register_on_generated(function(minp, maxp, blockseed)
 	local area = VoxelArea:new{MinEdge = emin, MaxEdge = emax}
 	vm:get_data(data)
 
-	for z = 0, 79 do
-		for y = 0, 79 do
-			for x = 0, 79 do
-				local pos = {
-					x = minp.x + x,
-					y = minp.y + y,
-					z = minp.z + z
-				}
-
-				local posi = area:index(pos.x, pos.y, pos.z)
-
-				if (pos.x >= -mg.size and pos.x <= mg.size) and (pos.z >= -mg.size and pos.z <= mg.size) then
-					if pos.y >= -mg.depth and pos.y <= -1 then
-						data[posi] = mg.blocks.dirt
-					elseif pos.y == 0 then
-						data[posi] = mg.blocks.grass
-					elseif pos.y == -mg.depth -1 then
-						data[posi] = mg.blocks.bedrock
-					end
-				elseif pos.y >= -mg.depth -1 and pos.y <= -3 then
-					if	(pos.x == -mg.size -1 and (pos.z >= -mg.size -1 and pos.z <= mg.size +1))
-					 or	(pos.x ==  mg.size +1 and (pos.z >= -mg.size -1 and pos.z <= mg.size +1))
-					 or	(pos.z == -mg.size -1 and (pos.x >= -mg.size -1 and pos.x <= mg.size +1))
-					 or	(pos.z ==  mg.size +1 and (pos.x >= -mg.size -1 and pos.x <= mg.size +1))
-					 or (pos.y == -3 and (pos.x > mg.size +1 or pos.x < -mg.size -1 or pos.z > mg.size +1 or pos.z < -mg.size -1)) then
-						data[posi] = mg.blocks.bedrock
-					end
-				elseif	pos.y >= 0 and (
-						(pos.x == -mg.size -1 and (pos.z >= -mg.size -1 and pos.z <= mg.size +1))
-					 or	(pos.x ==  mg.size +1 and (pos.z >= -mg.size -1 and pos.z <= mg.size +1))
-					 or	(pos.z == -mg.size -1 and (pos.x >= -mg.size -1 and pos.x <= mg.size +1))
-					 or	(pos.z ==  mg.size +1 and (pos.x >= -mg.size -1 and pos.x <= mg.size +1))) then
-					data[posi] = mg.blocks.invisible_bedrock
-				elseif pos.y < 0 and pos.y > -3 then
-					data[posi] = mg.blocks.solid_water
+	for z = minp.z, maxp.z do
+	for y = minp.y, maxp.y do
+		local posi = area:index(minp.x, y, z)
+		for x = minp.x, maxp.x do
+			if (x >= -mg.size and x <= mg.size) and (z >= -mg.size and z <= mg.size) then
+				if y >= -mg.depth and y <= -1 then
+					data[posi] = mg.blocks.dirt
+				elseif y == 0 then
+					data[posi] = mg.blocks.grass
+				elseif y == -mg.depth -1 then
+					data[posi] = mg.blocks.bedrock
 				end
+			elseif y >= -mg.depth -1 and y <= -3 then
+				if		(x == -mg.size -1 and (z >= -mg.size -1 and z <= mg.size +1))
+					or	(x ==  mg.size +1 and (z >= -mg.size -1 and z <= mg.size +1))
+					or	(z == -mg.size -1 and (x >= -mg.size -1 and x <= mg.size +1))
+					or	(z ==  mg.size +1 and (x >= -mg.size -1 and x <= mg.size +1))
+					or (y == -3 and (x > mg.size +1 or x < -mg.size -1 or z > mg.size +1 or z < -mg.size -1)) then
+					data[posi] = mg.blocks.bedrock
+				end
+			elseif y >= 0 and (
+						(x == -mg.size -1 and (z >= -mg.size -1 and z <= mg.size +1))
+					or	(x ==  mg.size +1 and (z >= -mg.size -1 and z <= mg.size +1))
+					or	(z == -mg.size -1 and (x >= -mg.size -1 and x <= mg.size +1))
+					or	(z ==  mg.size +1 and (x >= -mg.size -1 and x <= mg.size +1))) then
+				data[posi] = mg.blocks.invisible_bedrock
+			elseif y < 0 and y > -3 then
+				data[posi] = mg.blocks.solid_water
 			end
+
+			posi = posi + 1
 		end
-	end
+	end end
 
 	vm:set_data(data)
 	vm:write_to_map()
