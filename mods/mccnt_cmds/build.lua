9ad7b1d4e1f1c75295a4d26a8d9c30f49a50f508
ROllerozxa <temporaryemail4meh+github@gmail.com>
2022-06-28 20:46:46 +0200

    Add /place

--- mods/mccnt_cmds/build.lua
+++ mods/mccnt_cmds/build.lua
@@ -22,3 +22,21 @@ minetest.register_on_dignode(function(pos, oldnode, digger)
 		minetest.set_node(pos, { name = digger:get_wielded_item():get_name() })
 	end
 end)
+
+
+-- /place
+minetest.register_chatcommand("place", {
+	description = "Place a stone block at your current position, useful for building high up.",
+	privs = { interact = true },
+	func = function(name, param)
+		local player = minetest.get_player_by_name(name)
+
+		local pos = vector.new(player:get_pos())
+		pos = vector.round(pos)
+
+		if minetest.get_node(pos).name == "air" then
+			minetest.place_node(pos, { name = "minecraft:stone" })
+			minetest.chat_send_player(name, yellow("Successfully placed block at position."))
+		end
+	end
+})
